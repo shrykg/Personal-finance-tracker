e4405fb03d9cec0f3c91ecd751c54a6d98db9fac
Shreyak Godala <shreyakgodala2909@gmail.com>
2023-04-13 15:51:05 -0400

    Added Validation file and mongo transaction data methods

---
+++ data/index.js
@@ -0,0 +1,3 @@
+import transactionDataFunctions from './transactions.js'
+
+export const transactionData = transactionDataFunctions
---
+++ routes/transactions.js
@@ -0,0 +1,4 @@
+import {Router} from 'express';
+const router = Router();
+import { transactionData } from '../data/index.js';
+import validation from '../validation.js';
