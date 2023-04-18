91f7ce3f02c6a0eecc962a8da7c8d4d7ab4252d8
Shreyak Godala <shreyakgodala2909@gmail.com>
2023-04-18 12:08:00 -0400

    reverted charts changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,5 @@
 import log_in_routes from './log_in_registration.js';
-<<<<<<< HEAD
-import chart_routes from './charts.js';
-=======
 import transactionRoutes from './transactions.js'
->>>>>>> d3a708567c5fb844b21e1d775ee7e87bda074d28
 import path from 'path';
 
 const constructorMethod = (app) => {
@@ -11,11 +7,7 @@ const constructorMethod = (app) => {
     app.use('/login', log_in_routes)
     app.use('/registration', log_in_routes)
     app.use('error', log_in_routes)
-<<<<<<< HEAD
-    app.use('/',chart_routes)
-=======
     app.use('/transactions', transactionRoutes)
->>>>>>> d3a708567c5fb844b21e1d775ee7e87bda074d28
 
     app.use('*', (req, res) => {
         res.status(404).render("error", { error_occured: "The URL Passed is invalid" });
--- seed.js
+++
@@ -1,7 +0,0 @@
-
-import { dbConnection, closeConnection } from "./config/mongoConnection.js";
-import * as transaction from './data/transactions.js';
-
-const db = await dbConnection();
-await db.dropDatabase();
-
