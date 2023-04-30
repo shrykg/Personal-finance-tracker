25cb5797007cfca19cad047629fd36d3b5ff6f30
Daksh Pruthi <dakshpruthi099@gmail.com>
2023-04-30 06:23:48 -0400

    added chart for quarterly expenses

--- public/charts/charts.js
+++ public/charts/charts.js
@@ -67,6 +67,46 @@ function prepareYearlyGrowthData(transactions)
     return growthData;
 }
 
+function prepareQuarterlyChartData(transactions)
+{
+    const quarterlyAmounts = new Map();
+  
+    transactions.forEach(transaction =>
+        {
+            const date = new Date(transaction.transaction_date);
+            const quarter = Math.floor((date.getMonth() / 3));
+            const year = date.getFullYear();
+            const yearQuarter = `${year}-Q${quarter + 1}`;
+            const category = transaction.category;
+        
+            if (!quarterlyAmounts.has(yearQuarter))
+            {
+                quarterlyAmounts.set(yearQuarter, new Map());
+            }
+  
+            const categoryAmounts = quarterlyAmounts.get(yearQuarter);
+            const currentCategoryAmount = categoryAmounts.get(category) || 0;
+            categoryAmounts.set(category, currentCategoryAmount + transaction.amount);
+        });
+  
+    const allCategories = new Set(transactions.map(transaction => transaction.category));
+    const quarterlyChartData = [['Year-Quarter', ...allCategories]];
+  
+    for (const [yearQuarter, categoryAmounts] of quarterlyAmounts)
+    {
+        const row = [yearQuarter];
+
+        allCategories.forEach(category => 
+            {
+                row.push(categoryAmounts.get(category) || 0);
+            }
+        );
+
+        quarterlyChartData.push(row);
+    }
+  
+    return quarterlyChartData;
+}
 function drawPieChart(transactions) 
 {
     const pieChartData = preparePieChartData(transactions);
@@ -97,10 +137,21 @@ function drawBarChart(transactions)
     });
 }
 
+function drawStackedColumnChart(transactions)
+{
+    const quarterlyChartData = prepareQuarterlyChartData(transactions);
+    const stackedColumnChart = new google.visualization.ColumnChart(document.getElementById('stackedColumnChart'));
+    stackedColumnChart.draw(google.visualization.arrayToDataTable(quarterlyChartData), 
+    {
+        title: 'Quarterly Expenses by Category (Stacked Column Chart)',
+        isStacked: true
+    });
+}
 function drawCharts() 
 {
     const transactions = JSON.parse(document.getElementById('transactions-data').textContent);
     drawPieChart(transactions);
     drawLineChart(transactions);
     drawBarChart(transactions);
+    drawStackedColumnChart(transactions);
 }
